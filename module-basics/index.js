562e60c54e5b251f270fbeee5658715a78c1f454
Maximal <maxim@infofree.ru>
2018-02-26 23:44:28 +0500

    lesson 3

--- module-basics/index.js
+++ module-basics/index.js
@@ -2,9 +2,15 @@ console.log('Starting app.')
 
 const fs = require('fs')
 const os = require('os')
+const _ = require('lodash')
 const notes = require('./notes')
 
-console.log('Result:', notes.add(9, -2))
+const filteredArray = _.uniq(['Max', 1, 'Max', 1, 2, 3, 4])
+console.log(filteredArray)
+
+// console.log(_.isString(true))
+
+// console.log('Result:', notes.add(9, -2))
 
 // const user = os.userInfo()
 //
