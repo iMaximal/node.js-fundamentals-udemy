b7d7940947751177d69e3c1a878bb9ba0e466fc9
Maximal <maxim@infofree.ru>
2018-02-27 01:28:57 +0500

    lesson 5

--- input-args/index.js
+++ input-args/index.js
@@ -1,14 +1,22 @@
 console.log('Starting app')
 
-const command = process.argv[2]
+const fs = require('fs')
+const _ = require('lodash')
+const yargs = require('yargs')
+const notes = require('./notes')
+
+const argv = yargs.argv
+const command = process.argv[2] // equal argv._[0]
 console.log('Command: ', command)
+// console.log('Process: ', process.argv)
+console.log('Yargs: ', argv)
 
 if (command === 'add') {
-  console.log('Adding new note')
+  notes.addNote(argv.title, argv.body)
 } else if (command === 'list') {
-  console.log('Listing all notes')
+  notes.getAll()
 } else if (command === 'read') {
-  console.log('Reading note')
+  notes.getNote(argv.title)
 } else if (command === 'remove') {
   console.log('Removing note')
 } else {
