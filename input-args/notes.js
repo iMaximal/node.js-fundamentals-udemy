f882fd705219bff8d017d8f10dca56cf7562767b
Maximal <maxim@infofree.ru>
2018-02-28 22:11:47 +0500

    Adding and Saving notes

--- input-args/notes.js
+++ input-args/notes.js
@@ -1,7 +1,27 @@
 console.log('Starting notes.js')
 
+const fs = require('fs')
+
 const addNote = (title, body) => {
-  console.log('Adding note', title, body)
+  let notes = []
+  const note = {
+    title,
+    body,
+  }
+
+  try {
+    const notesString = fs.readFileSync('notes-data.json')
+    notes = JSON.parse(notesString)
+  } catch (e) {
+
+  }
+
+  const deDuplicatedNotes = notes.filter((note) => note.title === title)
+
+  if (deDuplicatedNotes.length === 0) {
+    notes.push(note)
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+  }
 }
 
 const getAll = () => {
--- module-basics/index.js
+++ module-basics/index.js
@@ -3,7 +3,7 @@ console.log('Starting app.')
 const fs = require('fs')
 const os = require('os')
 const _ = require('lodash')
-const notes = require('./notes')
+// const notes = require('./notes')
 
 const filteredArray = _.uniq(['Max', 1, 'Max', 1, 2, 3, 4])
 console.log(filteredArray)
